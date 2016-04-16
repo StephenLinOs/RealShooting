63e7ee533deabcda36c0c4b1ad97205471b01967
Stephen Lin <stephen-os@stephen-osdemacbook-pro.local>
2016-04-16 22:52:01 +0800

    Initial Commit

---
+++ RealAttackTests/RealAttackTests.swift
@@ -0,0 +1,36 @@
+//
+//  RealAttackTests.swift
+//  RealAttackTests
+//
+//  Created by Stephen-OS on 2016/4/16.
+//  Copyright © 2016年 Stephen-OS. All rights reserved.
+//
+
+import XCTest
+@testable import RealAttack
+
+class RealAttackTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
